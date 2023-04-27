316fbdd7ff8d3fdb4901ed112c446a361ce1f107
dgsglryz <dogusguleryuz@gmail.com>
2023-04-27 09:21:25 -0700

    Initial Commit

---
+++ Assignment5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment5
+//
+//  Created by Helin Akın on 27.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
